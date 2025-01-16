12350849b5908dccddb95248f4afef4bac931a8b
JOOWON <wer07032@naver.com>
2025-01-16 21:34:03 +0900

    git init

---
+++ template_lab/src/test/java/com/dali186/template_lab/TemplateLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dali186.template_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
