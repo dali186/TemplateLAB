0a4e6c9ff8839ce47f5ed913ed2970db9517a471
JOOWON <wer07032@naver.com>
2025-01-19 21:52:32 +0900

    feat : [Quill] Implement Editor File Control

--- template_lab/src/main/java/com/dali186/template_lab/quill/dto/BoardDto.java
+++ template_lab/src/main/java/com/dali186/template_lab/quill/dto/BoardDto.java
@@ -1,9 +1,9 @@
 package com.dali186.template_lab.quill.dto;
 
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 public class BoardDto {
@@ -13,4 +13,12 @@ public class BoardDto {
     private String author;
     private String createdAt;
     private String updatedAt;
+
+    private Boolean isAttachFileChanged;
+    private MultipartFile[] attachFiles;
+    private List<BoardFileDto> attachFilesMetaData;
+
+    private Boolean isEditorFileChanged;
+    private MultipartFile[] editorFiles;
+    private List<BoardFileDto> editorFilesMetaData;
 }
--- template_lab/src/main/java/com/dali186/template_lab/quill/mapper/QuillMapper.java
+++ template_lab/src/main/java/com/dali186/template_lab/quill/mapper/QuillMapper.java
@@ -1,7 +1,9 @@
 package com.dali186.template_lab.quill.mapper;
 
 import com.dali186.template_lab.quill.dto.BoardDto;
+import com.dali186.template_lab.quill.dto.BoardFileDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -10,5 +12,8 @@ public interface QuillMapper {
     List<BoardDto> getBoardList();
     BoardDto getBoardById(int boardId);
     Integer registerBoard(BoardDto boardDto);
-    Integer modifyBoard(int boardId);
+    Integer registerFile(BoardFileDto fileDto);
+    Integer modifyBoard(BoardDto boardDto);
+    Integer deleteBoardById(int boardId);
+    List<BoardFileDto> getBoardFileList(@Param("boardId") String boardId, @Param("type") String type);
 }
